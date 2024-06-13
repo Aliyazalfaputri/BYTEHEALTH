5a92f73ae3df59b3d9257109f394485cea528b19
pradyaheningpertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2024-06-13 22:06:51 +0700

    ubah dtabs

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","bytehealth (1)");
+    $database= new mysqli("localhost","root","","bytehealth");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
