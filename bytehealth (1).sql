3567547fed66c4f4205daaac57ed9eb285e8f12c
Aliyazalfaputri <aliyazalfa186@gmail.com>
2024-06-13 20:09:57 +0700

    database baru

---
+++ bytehealth (1).sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2024 at 03:08 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bytehealth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `aemail` varchar(255) NOT NULL,
+  `apassword` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aemail`, `apassword`) VALUES
+('admin@edoc.com', '123'),
+('adminbytehealth@yahoo.com', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appoid` int(11) NOT NULL,
+  `pid` int(10) DEFAULT NULL,
+  `apponum` int(3) DEFAULT NULL,
+  `scheduleid` int(10) DEFAULT NULL,
+  `appodate` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appoid`, `pid`, `apponum`, `scheduleid`, `appodate`) VALUES
+(3, 3, 1, 10, '2024-06-06'),
+(2, 3, 1, 9, '2024-06-06'),
+(4, 6, 1, 13, '2024-06-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `docid` int(11) NOT NULL,
+  `docemail` varchar(255) DEFAULT NULL,
+  `docname` varchar(255) DEFAULT NULL,
+  `docpassword` varchar(255) DEFAULT NULL,
+  `docnic` varchar(15) DEFAULT NULL,
+  `doctel` varchar(15) DEFAULT NULL,
+  `specialties` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`docid`, `docemail`, `docname`, `docpassword`, `docnic`, `doctel`, `specialties`) VALUES
+(4, 'abla@gmail.com', 'dr. Abla Ghanie, Sp.THT', '123', '1003004005', '0822349350', 3),
+(2, 'deeetttaaa@gmail.com', 'Drg. Deta Sari, Sp.BM', '123', '085156826366', '085156826366', 2),
+(3, 'angelina@gmail.com', 'Angelina Bangun, S.Ked', '123', '081030040', '0822349350', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `pid` int(11) NOT NULL,
+  `pemail` varchar(255) DEFAULT NULL,
+  `pname` varchar(255) DEFAULT NULL,
+  `ppassword` varchar(255) DEFAULT NULL,
+  `paddress` varchar(255) DEFAULT NULL,
+  `pdob` date DEFAULT NULL,
+  `ptel` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`pid`, `pemail`, `pname`, `ppassword`, `paddress`, `pdob`, `ptel`) VALUES
+(6, 'aliyaz18@gmail.com', 'aliya zalfa', 'aliya18', 'kedaton', '2004-06-18', '085156826366'),
+(7, 'deeetttaaa@gmail.com', 'deta sari', '123456', 'kopi', '2005-02-09', '087843847001'),
+(8, 'pradyahening@gmail.com', 'pradya hening', '123456', 'metro', '2004-02-09', '087834966959');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `scheduleid` int(11) NOT NULL,
+  `docid` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `scheduledate` date DEFAULT NULL,
+  `scheduletime` time DEFAULT NULL,
+  `nop` int(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`scheduleid`, `docid`, `title`, `scheduledate`, `scheduletime`, `nop`) VALUES
+(13, '2', 'Poli Gigi dan Mulut', '2024-06-06', '11:10:00', 12),
+(2, '1', '1', '2022-06-10', '20:36:00', 1),
+(3, '1', '12', '2022-06-10', '20:33:00', 1),
+(4, '1', '1', '2022-06-10', '12:32:00', 1),
+(5, '1', '1', '2022-06-10', '20:35:00', 1),
+(6, '1', '12', '2022-06-10', '20:35:00', 1),
+(11, '3', 'Poli Umum 1', '2024-06-06', '08:20:00', 10),
+(8, '1', '12', '2022-06-10', '13:33:00', 1),
+(12, '4', 'Poli THT', '2024-06-06', '09:00:00', 120),
+(14, '4', 'POLI THT', '2024-06-13', '18:00:00', 100),
+(15, '3', 'POLI UMUM 1', '2024-06-13', '18:01:00', 20),
+(16, '2', 'POLI GIGI DAN MULUT', '2024-06-13', '19:01:00', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `id` int(2) NOT NULL,
+  `sname` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `specialties`
+--
+
+INSERT INTO `specialties` (`id`, `sname`) VALUES
+(1, 'umum'),
+(2, 'Gigi dan Mulut'),
+(3, 'THT'),
+(4, 'UGD'),
+(5, 'Labolatorium'),
+(6, 'Kesehatan Ibu dan Anak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webuser`
+--
+
+CREATE TABLE `webuser` (
+  `email` varchar(255) NOT NULL,
+  `usertype` char(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `webuser`
+--
+
+INSERT INTO `webuser` (`email`, `usertype`) VALUES
+('admin@edoc.com', 'a'),
+('abla@gmail.com', 'd'),
+('patient@edoc.com', 'p'),
+('emhashenudara@gmail.com', 'p'),
+('aliyazalfa186@gmail.com', 'p'),
+('deeetttaaa@gmail.com', 'd'),
+('aliyazalfa151@gmail.com', 'p'),
+('aliya186@gmail.com', 'p'),
+('aliyaz18@gmail.com', 'p'),
+('angelina@gmail.com', 'd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aemail`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appoid`),
+  ADD KEY `pid` (`pid`),
+  ADD KEY `scheduleid` (`scheduleid`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`docid`),
+  ADD KEY `specialties` (`specialties`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`scheduleid`),
+  ADD KEY `docid` (`docid`);
+
+--
+-- Indexes for table `webuser`
+--
+ALTER TABLE `webuser`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `docid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `scheduleid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
